78e7b475f8888ae2fb05c516ed1cb9777bb0b5e0
nangongyibin <nangongyibin@gmail.com>
2020-06-02 11:23:29 +0800

    js修改文本、属性、样式及日志的打印

---
+++ js/js.js
@@ -0,0 +1,10 @@
+function outsideJs() {
+    document.getElementById('ngybP').innerHTML = '我的第一个JavaScripts';
+}
+
+function outPutLog() {
+    // document.getElementById('ngybP').innerHTML = 5+6;
+    // document.write(5+6);
+    // window.alert(5+6);
+    console.log(5+6);
+}
