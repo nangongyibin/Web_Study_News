c6d1383338b1717b8a013a0de499625b276c692f
nangongyibin <nangongyibin@gmail.com>
2020-06-03 09:07:47 +0800

    变量及运算

--- js/js.js
+++ js/js.js
@@ -8,3 +8,20 @@ function outPutLog() {
     // window.alert(5+6);
     console.log(5+6);
 }
+
+function statement() {
+    var x,y,z; 
+    x = 22;
+    y =11; 
+    z =x+y;
+    document.getElementById('ngybP').innerHTML = z;
+}
+
+function grammar() {
+    var x=7,y = 8,i = 10;
+    var z; 
+    z = x; 
+    z = (x+y)*i;
+    z =z+x;
+    document.getElementById('ngybP').innerHTML = z;
+}
